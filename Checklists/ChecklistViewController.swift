f1387ef9f9637d585bf234fa62171e63ae67c9ba
CelesTech03 <celeste.urena@stu.bmcc.cuny.edu>
2022-10-05 16:25:15 -0400

    Fixed issue with checkmarks not displaying correctly after scrolling

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -8,18 +8,55 @@
 import UIKit
 
 class ChecklistViewController: UITableViewController {
+    let row0text = "Walk the dog"
+    let row1text = "Brush teeth"
+    let row2text = "Learn iOS development"
+    let row3text = "Soccer practice"
+    let row4text = "Eat ice cream"
+    
+    var row0checked = false
+    var row1checked = true
+    var row2checked = true
+    var row3checked = false
+    var row4checked = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
+    // Makes checkmark visible or invisible according to "row checked" var
+    func configureCheckmark(
+        for cell: UITableViewCell,
+        at indexPath: IndexPath) {
+            var isChecked = false
+            
+            if indexPath.row == 0 {
+                isChecked = row0checked
+            } else if indexPath.row == 1 {
+                isChecked = row1checked
+            } else if indexPath.row == 2 {
+                isChecked = row2checked
+            } else if indexPath.row == 3 {
+                isChecked = row3checked
+            } else if indexPath.row == 4 {
+                isChecked = row4checked
+            }
+            
+            if isChecked {
+                cell.accessoryType = .checkmark
+            } else {
+                cell.accessoryType = .none
+            }
+            
+        }
+    
     // MARK: - Table View Data Source
     // Data source protocol for table view
     override func tableView(
         _ tableView: UITableView,
         numberOfRowsInSection section: Int) -> Int { // Method signature
-            return 100
+            return 5
         }
     
     override func tableView(
@@ -33,19 +70,20 @@ class ChecklistViewController: UITableViewController {
             // Asks table view cell for the view with tag 1000
             let label = cell.viewWithTag(1000) as! UILabel
             
-            if indexPath.row % 5 == 0 {
-                label.text = "Walk the dog"
-            } else if indexPath.row % 5 == 1 {
-                label.text = "Brush my teeth"
-            } else if indexPath.row % 5 == 2 {
-                label.text = "Learn iOS development"
-            } else if indexPath.row % 5 == 3 {
-                label.text = "Soccer practice"
-            } else if indexPath.row % 5 == 4 {
-                label.text = "Eat ice cream"
+            if indexPath.row == 0 {
+                label.text = row0text
+            } else if indexPath.row == 1 {
+                label.text = row1text
+            } else if indexPath.row == 2 {
+                label.text = row2text
+            } else if indexPath.row == 3 {
+                label.text = row3text
+            } else if indexPath.row == 4 {
+                label.text = row4text
             }
             // End of code block
             
+            configureCheckmark(for: cell, at: indexPath)
             return cell
         }
     
@@ -54,11 +92,21 @@ class ChecklistViewController: UITableViewController {
         _ tableView: UITableView,
         didSelectRowAt indexPath: IndexPath) {
             if let cell = tableView.cellForRow(at: indexPath) {
-                if cell.accessoryType == .none {
-                    cell.accessoryType = .checkmark
-                } else {
-                    cell.accessoryType = .none
+                
+                // toggles the "checked" state from false to true and vice versa
+                if indexPath.row == 0 {
+                    row0checked.toggle()
+                } else if indexPath.row == 1 {
+                    row1checked.toggle()
+                } else if indexPath.row == 2 {
+                    row2checked.toggle()
+                } else if indexPath.row == 3 {
+                    row3checked.toggle()
+                } else if indexPath.row == 4 {
+                    row4checked.toggle()
                 }
+                
+                configureCheckmark(for: cell, at: indexPath)
             }
             tableView.deselectRow(at: indexPath, animated: true)
         }
--- README.md
+++ README.md
@@ -10,6 +10,7 @@ The following functionality is completed:
 - [x] Put a table view on the app's screen
 - [x] Put data into that table view
 - [x] Allow the user to tap a row in the table to toggle a checkmark on and off
+- [ ] Allow the user to store to-do information
 
 ## Video Walkthrough
 Here's a walkthrough of implemented user stories:
